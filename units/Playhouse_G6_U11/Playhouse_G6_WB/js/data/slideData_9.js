bc0810fc0fd37bc91e1aee4e127edac02e794c7d
waleed-alqadi <developer3@bytechjo.com>
2026-09-10 12:27:55 +0300

    edit unit 11

--- units/Playhouse_G6_U11/Playhouse_G6_WB/js/data/slideData_9.js
+++ units/Playhouse_G6_U11/Playhouse_G6_WB/js/data/slideData_9.js
@@ -19,7 +19,7 @@ var stereo_data = {
   ],
 }
 var reading_data = {
-    "mainTitle": "../images/pages/sb-icons/writing.jpg",
+    "mainTitle": "../images/pages/sb-icons/WRITING.jpg",
     "mainTitleIcon": "",
     "mainTitleIconPos": {"right": "100px"},
     "mainTitleAudio": "../audios/p58/WRITING.mp3",
